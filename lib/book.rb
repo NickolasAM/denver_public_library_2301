c36a001c0198a5e4859d824023ae9e60c23a4aaf
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-24 09:23:43 -0700

    Finished iteration1

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,22 @@
 class Book
-  
+  attr_reader :title,
+              :author,
+              :publication_year
+  def initialize(book)
+    @title = title
+    @author = author
+    @publication_year = publication_year
+  end
+
+  def title
+    @title = "To Kill a Mockingbird"
+  end
+
+  def author
+    @author = "Harper Lee"
+  end
+
+  def publication_year
+    @publication_year = "1960"
+  end
 end
